60f09835f34c0aaa3cef1c5a030b97d78f43d1ca
halinakh <galinahomik@gmail.com>
2021-02-15 13:43:13 +0300

    redis

--- yii/config/console.php
+++ yii/config/console.php
@@ -26,6 +26,12 @@ $config = [
             ],
         ],
         'db' => $db,
+        'redis' => [
+            'class' => 'yii\redis\Connection',
+            'hostname' => 'localhost',
+            'port' => 6379,
+            'database' => 0,
+        ],
     ],
     'params' => $params,
     /*
